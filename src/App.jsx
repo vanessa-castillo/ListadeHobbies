56569ad23d50ca391b7a7c1f52ddbcb7f9d611c2
vanessa-castillo <castillogarciav11@gmail.com>
2023-06-06 20:20:59 -0600

    lista de hobbies

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+function App() {
+  const name= "Vanessa Castillo Garcia";
+  return (
+    <div>
+      <h3>Lista de Hobbies de {name}</h3>
+      <ul>
+        <li>jugar futbol</li>
+        <li>ver series y peliculas</li>
+        <li>cocinar</li>
+      </ul>
+    </div>
+  )
+}
+
+export default App
